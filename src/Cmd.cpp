052f8b1cc3c23da41ce1ef872b844c5959248ab4
Piotr Szerszynski <13pszerszynski@sandbachschool.org>
2019-06-21 12:14:13 +0000

    cmd parsing

--- src/Cmd.cpp
+++ src/Cmd.cpp
@@ -94,17 +94,33 @@ std::string ExecuteCommand(const struct Command& com) {
 	return func(com.args);
 }
 
+using namespace Parser;
+
 INumber Parser::l_number;
 std::string Parser::l_string;
+std::string Parser::ErrorMsg = "";
 
 std::stringstream l_sstr;
-int l_char;
+int l_char, last_token;
+
 void Parser::SetupLexer(std::string str) {
 	l_sstr = std::stringstream(str);
-	l_char = sstr.get();
+	l_char = l_sstr.get();
+}
+
+int Parser::GetCurrentToken() {
+	return last_token;
 }
 
 int Parser::GetNextToken() {
+	last_token = __GetNextToken();
+	if (last_token == ERROR) error_flag = true;
+	return last_token;
+}
+
+int Parser::__GetNextToken() {
+	error_flag = false;
+
 	// ignore whitespaces
 	while (std::isspace(l_char)) {
 		l_char = l_sstr.get();
@@ -115,58 +131,158 @@ int Parser::GetNextToken() {
 	
 	// if identifier
 	if (std::isalpha(l_char)) {
-		Parser::l_string = "";
-
-		while (std::isalnum(l_char)
-		       || l_char = '_' || l_char == '-')
-		{
-			Parser::l_string += l_char;
-			l_char = l_sstr.get();
-		}
-
-		return TOK_IDENTIFIER;
+		return LexIdentifier();
 	}
 
 	// if number
 	if (std::isdigit(l_char) || l_char == '-') {
-		std::string str;
-
-		bool is_int = true;
-		do {
-			str += l_char;
-			l_char = l_sstr.get();	
-			if (l_char == '.') is_int = false;
-		} while (std::isdigit(l_char) || l_char == '.');
-
-		try {
-			if (is_int)
-				l_number = INumber(std::stoll(str));
-			else
-				l_number = INumber(std::stod(str));
-		} catch (...) {
-			return ERROR;
-		}
-
-		return TOK_NUMBER;
+		return LexNumber();
 	}
 
 	// if string
 	if (l_char == '"') {
-		Parser::l_string = "";
-
-		do {
-			Parser::l_string += l_char;
-			l_char == l_sstr.get();
-			if (l_sstr.eof()) return ERROR;
-		} while (l_char != '"') // matching "
-
-		return TOK_STRING;
+		return LexString();
 	}
 
 	// return any characters not recognised as tokens
-	return l_char;
+	int old_char = l_char;
+	l_char = l_sstr.get();
+	return old_char;
 }
 
-int Parser::ParseCommand(const std::string& string, Command& com) {
-	return 1;	
+int Parser::LexIdentifier() {
+	Parser::l_string = "";
+	while (std::isalnum(l_char) 
+	       || l_char == '_' || l_char == '-')
+	{
+		Parser::l_string += l_char;
+		l_char = l_sstr.get();
+	}
+
+	l_char = l_sstr.get();
+	return TOK_IDENTIFIER;
+}
+
+int Parser::LexNumber() {
+	std::string str;
+
+	bool is_int = true;
+	do {
+		str += l_char;
+		l_char = l_sstr.get();	
+		if (l_char == '.') is_int = false;
+	} while (std::isdigit(l_char) || l_char == '.');
+
+	try {
+		if (is_int)
+			l_number = INumber(std::stoll(str));
+		else
+			l_number = INumber(std::stod(str));
+	} catch (...) {
+		return ERROR;
+	}
+
+	l_char = l_sstr.get();
+	return TOK_NUMBER;
+}
+
+int Parser::LexString() {
+	Parser::l_string = "";
+
+	while (true) {
+		l_char = l_sstr.get();
+		if (l_sstr.eof()) return ERROR; // no matching "
+		if (l_char == '"') break;       // matching " found
+			
+		l_string += l_char;
+	}
+
+	l_char = l_sstr.get();
+	return TOK_STRING;
+}
+
+int Parser::ParseCommand(const std::string& string, Command& _com) {
+	Command command; // do not overwrite _com in case of error
+
+	ErrorMsg = "";
+	error_flag = false;
+
+	int tok = GetNextToken();
+	// command must begin with command name 
+	if (tok != TOK_IDENTIFIER) {
+		ErrorMsg = "Command must begin with command identifier";
+		return 0;
+	}
+
+	command.command = Parser::l_string;
+
+	GetNextToken();
+	while ((tok = GetCurrentToken()) != TOK_EOF) {
+		Argument arg = ParseArgument();
+		if (error_flag) return 0;
+		command.args.push_back(std::move(arg));
+	}
+
+	_com = command;
+	return 1;
+}
+
+bool error_flag = false;;
+Argument Parser::ParseArgument() {
+	switch (GetCurrentToken()) {
+	case TOK_NUMBER:
+		return ParseNumber();
+	case TOK_STRING:
+		return ParserString();
+	case TOK_IDENTIFIER:
+		return ParseIdentifier();
+	case '$':
+		return ParserCVar();
+	default:
+		ErrorMsg = "Unexpected token in command";
+		error_flag = true;
+		return Argument();
+	}
+}
+
+Argument Parser::ParseNumber() {
+	Argument arg(Parser::l_number);
+	GetNextToken();
+	return arg;
+}
+
+Argument Parser::ParseString() {
+	Argument arg(ARG_STRING, Parser::l_string);
+	GetNextToken();
+	return arg;
+}
+
+Argument Parser::ParseIdentifier() {
+	std::string id = Parser::l_string;
+	if (GetNextToken() != ':') {
+		ErrorMsg = "Expected ':' after argument label";
+		error_flag = true;
+		return Argument();
+	}
+
+	if (GetNextToken() == TOK_IDENTIFIER) {
+		ErrorMsg = "An argument cannot have two labels";
+		error_flag = true;
+		return Argument();
+	} else {
+		Argument arg = ParseArgument();
+		arg.label = id;
+		return arg;
+	}
+}
+
+Argument Parser::ParseCVar() {
+	GetNextToken(); // eat '$'
+	if (GetNextToken() != TOK_IDENTIFIER) {
+		ErrorMsg = "A CVar name can only be a string";
+		error_flag = true;
+		return Argument();
+	}
+
+	return Argument(ARG_CVAR, Parser::l_string);
 }
--- src/main.cpp
+++ src/main.cpp
@@ -3,7 +3,32 @@
 #include <iostream>
 
 int main(int argc, char ** argv) {
-	if (Core.Init()) {
+	std::string com = "test_command 50 128.0 \"hello\"";
+	
+	Parser::SetupLexer(com);
+	while (1) {
+		int tok = Parser::GetNextToken();
+		if (tok == Parser::TOK_EOF) break;
+
+		switch (tok) {
+		case Parser::TOK_STRING:
+			std::cout << "TOK_STRING: \"" << Parser::l_string
+				<< "\"" << std::endl;
+			break;
+		case Parser::TOK_IDENTIFIER:
+			std::cout << "TOK_IDENTIFIER: " << Parser::l_string
+				<< std::endl;
+			break;
+		case Parser::TOK_NUMBER:
+			std::cout << "TOK_NUMBER: " <<
+				(Parser::l_number.type == NUM_INT ?
+				 "int " + std::to_string(Parser::l_number.ToInt()) : 
+				 "double " + std::to_string(Parser::l_number.ToFloat())) << std::endl;
+		}
+	}
+
+	return 0;
+	/*if (Core.Init()) {
 		Log::Error("QUITTING");
 		Core.Quit();
 		return -1;
@@ -11,5 +36,5 @@ int main(int argc, char ** argv) {
 		Core.MainLoop();
 		Core.Quit();
 		return 0;
-	}
+	}*/
 }
