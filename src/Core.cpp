87f83a570276d8b322c675ae913d5695eab6edea
pszerszynski <alienmode1337@gmail.com>
2019-06-21 10:41:35 +0000

    not

--- src/Core.cpp
+++ src/Core.cpp
@@ -13,6 +13,18 @@ int Core::Init(InitParameters init) {
 		return -1;
 	}
 
+	int flags = MIX_INIT_OGG | MIX_INIT_MP3;
+	int initted = Mix_Init(flags);
+	if ( (initted&flags) != flags) {
+		Log::ErrorMix();
+		return -1;
+	}
+
+	if (Mix_OpenAudio(44100, MIX_DEFAULT_FORMAT, 2, 1024) == -1) {
+		Log::ErrorMix();
+		return -1;
+	}
+
 	window = SDL_CreateWindow(init.win_title.c_str(), SDL_WINDOWPOS_UNDEFINED,
 	  SDL_WINDOWPOS_UNDEFINED, init.win_w, init.win_h, init.win_flags);
 	if (window == nullptr) {
@@ -34,6 +46,9 @@ int Core::Init(InitParameters init) {
 void Core::Quit() {
 	Media.FreeMedia();
 
+	Mix_CloseAudio();
+	Mix_Quit();
+
 	TTF_Quit();
 
 	SDL_DestroyRenderer(renderer);
@@ -52,8 +67,16 @@ void Core::HandleEvents() {
 }
 
 void Core::MainLoop() {
+	bool intro = true;
 	while (going) {
 		HandleEvents();
+
+		if (intro) {
+			intro=false;
+			auto chunk = Media.GetChunk("sfx/notification.wav");
+			if (chunk != nullptr)
+				Mix_PlayChannel(-1, chunk, 0);
+		}
 
 		MainRender();
 	}
